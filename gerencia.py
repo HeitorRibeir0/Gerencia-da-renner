e6edd879cec32447f147ea5b6b1ee18a298aaddc
Heitor Ribeiro <heitorrmatos0159@gmail.com>
2024-06-13 20:05:30 -0300

    Atualização do código

--- gerencia.py
+++ gerencia.py
@@ -1,38 +1,35 @@
 """Desenvolva um sistema de gerenciamento de estoque para uma loja de roupas. O sistema deve permitir o cadastro de produtos, realizar vendas, adicionar novos estoques e gerar relatórios sobre a quantidade de cada produto disponível e o total de vendas realizadas."""
-'''ESTOQUE tem que ter uma lista com os itens no estoque, onde tem que ter o nome do item, e a quantidade. FUNÇÕES: cadastro, realizar venda, total de vendas.
+'''estoque:produto,quantidade /funções: add_produto/venda/exibir_produto/total_vendas
 '''
 
 class Estoque:
-    def __init__(self,produto,qnt):
-        self.produto = produto
-        self.qnt = qnt
+    def __init__(self):
         self.produtos = []
-        self.disponivel = True
 
-    def add_prod(self,produto):
-        self.produtos.append(produto)
-
-    def disponibilidade(self):
-        return 'Disponível' if self.disponivel else 'Indisponível'
+    def add_produto(self,produto, qnt):
+        self.produtos.append({'Produto': produto, 'Quantidade': qnt, 'Disponibilidade': True})
     
+    def vender_prod(self, nome_produto, qnt_venda):
+        for p in self.produtos:
+            if p['produto'] == nome_produto:
+                if p['qnt'] >= qnt_venda:
+                    p['qnt'] -= qnt_venda
+                    print(f"{qnt_venda} unidade(s) de {nome_produto} vendida(s).")
+                    if p['qnt'] == 0:
+                        p['disponibilidade'] = False
+                else:
+                    print(f"Quantidade insuficiente de {nome_produto} para venda.")
+                return
+        print(f"Produto {nome_produto} não encontrado no estoque.")
+
     def exibir_produto(self):
-        lista_produtos = ', '.join(self.produtos)
-        disponibilidades = self.disponibilidade()
-        return f'Produtos:\n{lista_produtos} {disponibilidades} {self.qnt}'
-    
-class Item_Estoque(Estoque):
-    def __init__(self,tipo,tamanho,cor):
-        self.tipo = tipo
-        self.tamanho = tamanho
-        self.cor = cor
-        self.roupas = []
+        return [f"Produto: {p['produto']}, Quantidade: {p['qnt']}, Disponibilidade: {p['disponibilidade']}" for p in self.produtos]
+
+estoque = Estoque('Camisa', 5)
+estoque.add_produto()
+estoque.exibir_produto
 
     
-
-estoque = Estoque('Camisa')
-item1 = Item_Estoque('Camisa','G','Preta')
-roupa1.cadastrar_prod('Camisa Preta G')
-print(roupa1.exibir_produto())
 
        
     
